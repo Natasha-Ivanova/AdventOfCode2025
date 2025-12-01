de1ffef8470d3407777a7cb80e23514159143714
Natasha <152193436+Natasha-Ivanova@users.noreply.github.com>
2025-12-01 11:44:21 -0500

    Refined Day 1 solution

--- day1.py
+++ day1.py
@@ -2,22 +2,31 @@
 
 directions = {"R": 1, "L": -1}
 with open("data/day1.txt") as f:
-    current = 50
+    pointer = 50  # where the pointer currently is
     p1res = 0
     p2res = 0
 
     for line in f.readlines():
         line = line.strip()
 
-        direction = line[0]
-        amount = int(line[1:])
-        # for every instruction, perform each tick manually and check if it's 0 at the current point
-        for i in range(amount):
-            current += directions[direction]
-            current = current % 100
-            if current == 0:
-                p2res += 1
-        if current == 0:
+        direction = directions[line[0]]
+        clicks = int(line[1:])
+        p2res += clicks // 100
+        clicks = clicks % 100
+        pointer += clicks*direction
+
+        if pointer % 100 == 0:
             p1res += 1
+        if pointer == 0:
+            p2res += 1
+        elif pointer < 0:  # passed 0 on a left turn
+            if clicks + pointer != 0: # check that pointer wasn't on 0 before turn
+                p2res += 1
+            pointer = 100 + pointer
+        elif pointer >= 100: # passed 0 on a right turn
+            p2res += 1
+            pointer = pointer - 100
+
+
     print("Part 1: " + str(p1res))
     print("Part 2: " + str(p2res))

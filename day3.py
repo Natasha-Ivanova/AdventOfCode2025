1ff46fc76f6879a500c2269dbd71a2bf626e12c4
Natasha <152193436+Natasha-Ivanova@users.noreply.github.com>
2025-12-03 08:12:31 -0500

    Day 3 part 2, refined part 1

--- day3.py
+++ day3.py
@@ -1,21 +1,32 @@
 # https://adventofcode.com/2025/day/3
 
+
+def buildJoltage(bank, size):
+    if size > 1:
+        current = max(bank[:-(size-1)])
+        currentIndex = bank.index(current)
+        return current*10**(size-1) +buildJoltage(bank[currentIndex+1:], size-1)
+    else:
+        return max(bank)
+
 def part1(banks):
     res = 0
     for bank in banks:
         intBank = [int(i) for i in list(bank)]
-        tens = max(intBank)
-        tensIndex = intBank.index(tens)
-        if tensIndex == len(intBank) - 1:
-            tens = max(intBank[:-1])
-            tensIndex = intBank.index(tens)
-        ones = max(intBank[tensIndex + 1:])
-        # print(tens*10+ones)
-        res += tens * 10 + ones
+        res += buildJoltage(intBank, 2)
+    return res
+
+def part2(banks):
+    res = 0
+    for bank in banks:
+        intBank = [int(i) for i in list(bank)]
+        currentRes = buildJoltage(intBank, 12)
+        res+=currentRes
     return res
 
 
-with (open("data/day3test.txt") as f):
+with (open("data/day3.txt") as f):
     lines = [line.strip() for line in f.readlines()]
 
 print("Part 1: "+str(part1(lines)))
+print("Part 2: "+str(part2(lines)))

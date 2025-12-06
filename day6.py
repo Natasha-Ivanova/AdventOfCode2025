cb55c76148ef0c6812ed223dcc21ecfaf9ddc1d6
Natasha <152193436+Natasha-Ivanova@users.noreply.github.com>
2025-12-06 08:50:18 -0500

    Day 6 part 2

--- day6.py
+++ day6.py
@@ -3,7 +3,8 @@
 def part1(lines):
     ops = lines.pop().split()
     problems = [[int(i) for i in line.split()] for line in lines]
-    problems = [[problems[i][j] for i in range(len(problems))] for j in range(len(problems[0]))] # swap rows and columns
+    problems = [[problems[i][j] for i in range(len(problems))] for j in
+                range(len(problems[0]))] # swap rows and columns
     res = 0
     for i, problem in enumerate(problems):
         op = ops[i]
@@ -17,11 +18,31 @@ def part1(lines):
     return res
 
 def part2(lines):
-    pass
+    problems = [[lines[i][j] for i in range(len(lines))] for j in
+                range(len(lines[0]))][::-1]
+    res = 0
+    temp_sum = 0
+    temp_mul = 1
+    for problem in problems:
+        current = "".join(problem[:-1]).replace(" ", "")
+        if current != "":
+            current = int(current)
+            temp_sum+=current
+            temp_mul*=current
+        op = problem[-1]
+        if op == "+":
+            res += temp_sum
+            temp_sum = 0
+            temp_mul = 1
+        elif op == "*":
+            res += temp_mul
+            temp_sum = 0
+            temp_mul = 1
+    return res
 
 
 with open("data/day6.txt") as f:
-    lines = [line.strip() for line in f.readlines()]
+    lines = [line[:-1] for line in f.readlines()]
 
-print("Part 1: "+str(part1(lines)))
+print("Part 1: "+str(part1(lines.copy())))
 print("Part 2: "+str(part2(lines)))

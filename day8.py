55bb219fa9f5b875a898ce4cc631151052886531
Natasha <152193436+Natasha-Ivanova@users.noreply.github.com>
2025-12-08 21:07:50 -0500

    Day 8 part 1

---
+++ day8.py
@@ -0,0 +1,77 @@
+# https://adventofcode.com/2025/day/8
+import math
+
+
+def euclidean_dist(p1, p2):
+    return math.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2+(p1[2]-p2[2])**2)
+    
+
+def part1(lines, num):
+    edges = dict()
+    points = dict()
+    clusters = []
+    cluster_points = []
+    for i,circuit in enumerate(lines):
+        points[i] = i
+        clusters.append(1)
+        cluster_points.append([i])
+        for j, temp_circuit in enumerate(lines[i+1:]):
+            dist = euclidean_dist(circuit, temp_circuit)
+            if dist in edges.keys():
+                edges[dist].append([i, j+i+1])
+            else:
+                edges[dist] = [[i, j+i+1]]
+    
+    
+    for i in range(num):
+        if len(edges) == 0:
+            break
+        min_edge_i = min(edges)
+        min_edge = edges[min_edge_i]
+        left = min_edge[0][0]
+        right = min_edge[0][1]  
+        if points[left] != points[right]:
+            first_cluster = points[left]
+            second_cluster = points[right]
+            if clusters[first_cluster] <= clusters[second_cluster]:
+                to_move = first_cluster
+                to_arrive = second_cluster
+            else:
+                to_move = second_cluster
+                to_arrive = first_cluster
+            cluster_points[to_arrive] += cluster_points[to_move]
+            for val in cluster_points[to_move]:
+                points[val] = to_arrive
+            cluster_points[to_move] = []
+            clusters[to_arrive]+=clusters[to_move]
+            clusters[to_move] = 0
+        if len(min_edge) == 1:
+            edges.pop(min_edge_i)
+        else:
+            edges[min_edge_i].pop(0)
+    res = 1
+    for size in sorted(clusters)[-3:]:
+        res*=size
+    return res
+        
+        
+    
+        
+    
+                
+            
+            
+            
+        
+
+
+
+def part2(lines, num):
+    pass
+
+
+with open("data/day8.txt") as f:
+    lines = [[int(i) for i in line.split(",")] for line in f.readlines()]
+
+print("Part 1: " + str(part1(lines, 1000)))
+print("Part 2: " + str(part2(lines, 10)))

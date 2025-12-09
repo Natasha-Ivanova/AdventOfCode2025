04a3c214513845fc66a5de6d9180a023fce6cc27
Natasha <152193436+Natasha-Ivanova@users.noreply.github.com>
2025-12-09 12:36:51 -0500

    Day 9 part 2 (using shapely)

--- day9.py
+++ day9.py
@@ -1,4 +1,6 @@
 # https://adventofcode.com/2025/day/9
+from shapely import Polygon
+from shapely import box
 
 def part1(lines):
     max_rect = 0
@@ -9,11 +11,19 @@ def part1(lines):
                            (abs(corner[1] - corner_2[1]) + 1)) 
     return max_rect
 
-
 def part2(lines):
-    pass
+    outer = Polygon(list(zip([line[0] for line in lines], [line[1] for line in lines])))
+    max_rect = 0
+    for i, corner in enumerate(lines):
+        for corner_2 in lines[i+1:]:
+            inner = box(min(corner[0], corner_2[0]), min(corner[1], corner_2[1]), max(corner[0], corner_2[0]), max(corner[1], corner_2[1]))
+            if outer.covers(inner):
+                max_rect = max(max_rect, 
+                               (abs(corner[0]-corner_2[0])+1)*
+                               (abs(corner[1] - corner_2[1]) + 1)) 
+    return max_rect
 
-
+        
 with open("data/day9.txt") as f:
     lines = [[int(i) for i in line.split(",")] for line in f.readlines()]
 

544d2b22257696e14af2fba649850b16a0bdee40
Natasha <152193436+Natasha-Ivanova@users.noreply.github.com>
2025-12-11 08:40:13 -0500

    Day 11 part 2

--- day11.py
+++ day11.py
@@ -1,26 +1,39 @@
 # https://adventofcode.com/2025/day/11
+from functools import cache
 
-def pathCount(connections, visited, current):
-    if current == "out":
+def pathCount(connections, visited, current, end):
+    if current == end:
         return 1
     else:
         visited = visited.copy()
         visited.add(current)
         res = 0
-        for connection in connections[current]:
-            if connection not in visited: 
-                res += pathCount(connections, visited, connection)
+        if current in connections.keys():
+            for connection in connections[current]:
+                if connection not in visited: 
+                    res += pathCount(connections, visited, connection, end)
+        return res                   
+                   
+@cache
+def pathCount2(current, dac, fft):
+    if current == "out":
+        return int(dac and fft)
+    else:    
+        if current == "dac":
+            dac = True
+        elif current == "fft":
+            fft = True
+        res = 0
+        for connection in lines[current]:
+                res += pathCount2(connection, dac, fft)  
         return res
-                
-            
-    
 
 def part1(connections):
-    return pathCount(connections, set(), "you")
+    return pathCount(connections, set(), "you", "out")
 
 
-def part2(connections):
-    pass
+def part2():
+    return pathCount2("svr", False, False)
 
 
 with open("data/day11.txt") as f:
@@ -28,4 +41,4 @@ with open("data/day11.txt") as f:
 
 
 print("Part 1: " + str(part1(lines)))
-print("Part 2: " + str(part2(lines)))
+print("Part 2: " + str(part2()))
